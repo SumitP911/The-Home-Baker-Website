aa6fcc8f70b17d8298f26a80a251fbe5ab861697
Sumit <sumitpatwardhan4k@gmail.com>
2024-08-29 14:02:04 +0530

    Committed remaining modifications

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -11,11 +11,11 @@ function Navigation({ onHomeClick, onProductsClick, onTestimonialsClick, onAbout
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <button onClick={onHomeClick}>Home</button>
-                            <button onClick={onProductsClick}>Products</button>
-                            <button onClick={onTestimonialsClick}>Testimonials</button>
-                            <button onClick={onAboutClick}>About</button>
-                            <button onClick={onContactClick}>Contact</button>
+                            <li><Link onClick={onHomeClick}>Home</Link></li>
+                            <li><Link onClick={onProductsClick}>Products</Link></li>
+                            <li><Link onClick={onTestimonialsClick}>Testimonials</Link></li>
+                            <li><Link onClick={onAboutClick}>About</Link></li>
+                            <li><Link onClick={onContactClick}>Contact</Link></li>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>

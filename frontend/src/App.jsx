64881ddba66ea4eda0583d219eca8e8c1829b134
Sumit <sumitpatwardhan4k@gmail.com>
2024-09-02 11:58:53 +0530

    Made modifications for scss files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 // Importing React, React Router DOM and React-Bootstrap
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './styles/global.scss';
 // import * as ReactBootstrap from 'react-bootstrap';
 
 // Importing all the pages and components
--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -1,17 +1,18 @@
 import React, { forwardRef } from 'react'
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import './Products.scss';
+// import yourImage from '';
 
 function Products() {
     return (
-        <>
+        <div className='ProductCategories'>
             <h2 className="d-flex justify-content-center">Product Categories</h2>
-            <Container className="d-flex justify-content-center">
-                <Row xs={1} sm={2} md={2} lg={4} className="g-4">
+            <Container className="d-flex justify-content-center my-5">
+                <Row xs={2} sm={2} md={2} lg={4} className="g-4">
                     {Array.from({ length: 4 }).map((_, idx) => (
                         <Col key={idx}>
                             <Card>
-                                <Card.Img variant="top" src=" " />
+                                <Card.Img variant="top" src="https://cdn.uengage.io/uploads/7175/image-8LDDGZ-1706080808.jpg" />
                                 <Card.Body>
                                     <Card.Title>Card title</Card.Title>
                                     <Card.Text>
@@ -25,7 +26,7 @@ function Products() {
                     ))}
                 </Row>
             </Container>
-        </>
+        </div>
     );
 }
 

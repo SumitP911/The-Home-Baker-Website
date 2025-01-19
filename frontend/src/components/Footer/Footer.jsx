8c220aafe475c8cbfb4dfc7ca04fea49b40789fe
Sumit <sumitpatwardhan4k@gmail.com>
2025-01-19 06:35:37 +0530

    Updated Footer section

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import logo from './logo-footer-main.jpeg';
 import './Footer.scss';
+
 
 function Footer() {
     return (
-        <footer>
-            <p>Footer Section</p>
-        </footer>
+        <Container fluid className="border-top pt-3 m-0">
+            <Row className="align-items-center justify-content-center text-center">
+                {/* Left Section: Brand Info */}
+                <div className="d-flex align-items-center justify-content-center text-center mb-4">
+                    <img src={logo} alt="Brand Logo" className="brand-logo me-3" style={{ width: '50px', height: '50px' }} />
+                    <div>
+                        <h3 className="brand-name mb-0">The Homebaker</h3>
+                        <p className="tagline mb-0">Cakes | Desserts | Brownies</p>
+                    </div>
+                </div>
+                <div className="copyrights m-0">
+                    <p>© 2025 All rights reserved</p>
+                </div>
+            </Row>
+        </Container>
     );
 }
 

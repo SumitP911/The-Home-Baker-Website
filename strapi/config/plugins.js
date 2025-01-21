dc6dbe512144c4f26420ea62fc306924a5c2f9e4
Sumit <sumitpatwardhan4k@gmail.com>
2025-01-21 12:41:31 +0530

    Updated plugins.js in strapi with jwtsecret

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,1 +1,7 @@
-module.exports = () => ({});
+module.exports = () => ({
+    "users-permissions": {
+        config: {
+            jwtSecret: process.env.JWT_SECRET || 'HTw3Twyj0/KemJ8TzK6j5w==',
+        },
+    },
+});

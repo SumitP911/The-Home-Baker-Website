34c27d29ce56df00ce97dd14256848a26335dac0
Sumit <sumitpatwardhan4k@gmail.com>
2025-01-21 17:50:28 +0530

    Added render backend url in middlewares.js

--- strapi/config/middlewares.js
+++ strapi/config/middlewares.js
@@ -5,9 +5,13 @@ module.exports = ({ env }) => [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000', 'http://192.168.1.34:3000'], // Add your frontend device's IP
+      origin: [
+        'http://localhost:3000', // Local frontend
+        'http://192.168.1.34:3000', // Local frontend on your device
+        'https://the-home-baker-website.onrender.com', // Render backend
+      ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      credentials: true,
+      credentials: true, // Enable if using cookies or headers
     },
   },
   'strapi::poweredBy',

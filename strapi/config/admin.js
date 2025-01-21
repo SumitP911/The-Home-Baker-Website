cb90c9bdcf4bda0115d01c256ecdb08912edc57c
Sumit <sumitpatwardhan4k@gmail.com>
2025-01-21 12:56:58 +0530

    Added transfer-token-salt in admin.js

--- strapi/config/admin.js
+++ strapi/config/admin.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'R1JrtzM5DjXns+NvdUwx5g=='),
     },
   },
   flags: {

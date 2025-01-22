2bc7c2ed9fcd532c4d6db1df1fd83213d2c4ec37
Sumit <sumitpatwardhan4k@gmail.com>
2025-01-22 17:48:10 +0530

    Updated middlewares.js for production

--- strapi/config/middlewares.js
+++ strapi/config/middlewares.js
@@ -7,8 +7,12 @@ module.exports = ({ env }) => [
     config: {
       origin: [
         'http://localhost:3000', // Local frontend
-        'http://192.168.1.34:3000', // Local frontend on your device
-        'https://the-home-baker-website.onrender.com', // Render backend
+        'http://192.168.1.34:3000', // Local frontend for other device
+        'https://thehomebaker.co.in',
+        'https://www.thehomebaker.co.in',
+        'https://the-home-baker-website.vercel.app/',
+        'https://the-home-baker-website-sumit-patwardhans-projects.vercel.app/',
+        'https://the-home-baker-website-git-main-sumit-patwardhans-projects.vercel.app/'
       ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       credentials: true, // Enable if using cookies or headers

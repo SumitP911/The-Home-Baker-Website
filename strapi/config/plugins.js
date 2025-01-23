e1fa059b616777b69540d9ac9e45831d63c52f61
Sumit <sumitpatwardhan4k@gmail.com>
2025-01-23 10:54:36 +0530

    Updated for Clourdflare object storage

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,7 +1,21 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
     "users-permissions": {
         config: {
-            jwtSecret: process.env.JWT_SECRET || 'HTw3Twyj0/KemJ8TzK6j5w==',
+            jwtSecret: env('JWT_SECRET', 'HTw3Twyj0/KemJ8TzK6j5w=='),
+        },
+    },
+    upload: {
+        config: {
+            provider: '@strapi/provider-upload-aws-s3',
+            providerOptions: {
+                accessKeyId: env('R2_ACCESS_KEY_ID'),
+                secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
+                region: 'auto',
+                endpoint: `https://${env('R2_ACCOUNT_ID')}.r2.cloudflarestorage.com`,
+                params: {
+                    Bucket: env('R2_BUCKET_NAME'),
+                },
+            },
         },
     },
 });
